056a3acf3434b565debdc42c4061ef6c2fbe00be
Haralambos <lampistsivikas@yahoo.com>
2019-09-18 10:59:32 -0700

    new message

--- liri.js
+++ liri.js
@@ -1,110 +1,131 @@
-var keys = require("./keys");
+                    
 require("dotenv").config();
-console.log(keys);
 
+//project vars
 
 var keys = require("./keys.js");
-//Sets up the user input for the function i and the content i seek.
-var userInput = process.argv[2];
-var ask = process.argv[3];
-// With this code i can literally find out where my favorite concert is playing
-var bandsInTown = function(bandInput) {
-    var Events = require('bandsintown-events');
-    if (bandInput === undefined) {
-        bandInput = "Error Occurence: ";
-    }
-    request ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error){
-        if(error) {
-            console.log("Error Occurence: " + error);
-        } else {
-            for(var i = 0; i < Events.length; i++){
-                console.log("City: " + Events[i].venue.city);
-                console.log("Venue: " + Events[i].venue.name);
-                console.log("Date: " + Events[i].datetime);
-            
+var fs = require("fs");
+var Spotify = require('node -spotify-api');
+var spotify = new Spotify(keys.spotify)
+var movieName = process.argv[3];
+var liriReturn = process.argv[2];
+var twitter = require('twitter');
+var client = new twitter(keys.twitter);
 
-//This function, in theory, would have allowed a user to find song info.
-function spotify() {
+//switches for various commands
+switch(liriReturn) {
+   case "my-tweets":
+       myTweets();
+       break;
 
-    var spotify = require('node-spotify-api');
- 
-    var spotify = new Spotify(keys.spotify)
- 
-    spotify.search({ type: 'track', query: value, limit: 1 }, function(err, data) {
-    // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        
-        // returning artist name
-        console.log(data.tracks.items[0].artists[0].name);
+case "spotify-this-song":
+     spotifyThisSong();
+     break;
 
-        // returning song's name 
-        console.log(data.tracks.items[0].name);
-        
-        // returning preview link of the song from Spotify
-        console.log(data.tracks.items[0].preview_url);
-        
-        // returning album that the song is from
-        console.log(data.tracks.items[0].album.name);        
-        });
-    };
+case "movie-this":
+    movieThis();
+    break;
 
-function movie() {
+case "do what it says"  :
+    dowhatitSays();
+    break;
 
-    // var omdb = require('omdb');
- 
-    var request = require('request');
-    request('http://www.omdbapi.com/?apikey=trilogy&plot=full&tomatoes=true&r=json&t='+ value, function (error, response, body) {
-    //   console.log('error:', error); // Print the error if one occurred
-    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-    const obj = JSON.parse(body);
+// instructions for first-time user lurking around on the command line//
+default: console.log("\n" + "type any command after 'node liri.js': " + "\n" +
+   "my-tweets" + "\n" +
+   "spotify-this-song 'any song title' " + "\n" +
+   "movie-this 'any-movie-title' " + "\n" +
+   "do-what-it-says" + "\n" +
+   "Use quotes for multiword titles!");
 
-    console.log(obj.Title);
-    console.log(obj.Year);
-    console.log(obj.imdbRating);
-    console.log(obj.tomatoRating);
-    console.log(obj.Country);
-    console.log(obj.Language);
-    console.log(obj.Plot);
-    console.log(obj.Actors);
- 
-    });
- 
-    if(!movie) {
-        return console.log('Movie not found!');
-    }
-    
 };
 
-function doIt() {
+//Command 1 my-tweets
+//errors who are from twitter are not defined or twitter is not a constructor//
+function myTweets() {
+    var params = { screen_name: 'WelcomeToParadise'};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+           for (var i = 0; i < tweets.length; i++) {
 
-    // fs is a core Node package for reading and writing files
-    var fs = require("fs");
-
-    
-    fs.readFile("random.txt", "utf8", function(error, data) {
-
-      // If the code experiences any errors it will log the error to the console.
-      if (error) {
-        return console.log(error);
-      }
-
-      // We will then print the contents of data
-        //console.log(data);
-
-      // Then split it by commas (to make it more readable)
-      var dataArr = data.split(",");
-
-      // We will then re-display the content as an array for later use.
-      console.log(dataArr[0]);
-      console.log(dataArr[1]);
-
-    //   cannot get the values of dataArr to run in commandline
-      var action = process.argv[2] = dataArr[0];
-      var value = process.argv[3] = dataArr[1];
-
+            console.log(tweets[i].text);
+           };
+        } else {
+            console.log("error: " + err);
+             return;
+        }
     });
 }
+
+//command 3 spotify this song
+// i need artist, song name, preview, album
+function spotifyThisSong(trackName) {
+    var trackName = process.argv[3];
+    if (!trackName) {
+        trackName = "The sign";
+    };
+    songRequest = trackName;
+    spotify.search({
+        type: "track",
+        query: SongRequest
+    },
+
+    function (err, data) {
+        if (!err) {
+            var trackinfo = data.tracks.items;
+            for (var i = 0; i < 5; i ++) { 
+                if (trackInfo[i] != undefined) {
+                    var spotifyResults = 
+                        "Artists: " + trackingInfo[i].artists[0].name + "\n" +
+                        "Song: " + trackInfo[i].name + "\n" +
+                        "Preview URL: " + trackInfo[i].preview_url + "\n"
+                        "Album: " + trackInfo[i].album.name + "\n"
+
+                        console.log(spotifyResults);
+                        console.log('');
+                };
+            };
+        } else {
+            console.log("error: " + err)
+            return;
+        }
+    });
+};
+//command 3 movie this
+// run a movie to the OMDB API with the movie specified
+function movieThis() {
+
+// using movieName from var list at top
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+request(queryUrl, function (error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+    // pull requested data in readable format
+    var myMovieData = JSON.parse(body);
+    var queryUrlResults =
+        "Title: " + myMovieData.Title + "\n" +
+        "Year: " + myMovieData.Year + "\n" +
+        "IMDB Rating: " + myMovieData.Ratings[0].Value + "\n" +
+        "Rotten Tomatoes Rating: " + myMovieData.Ratings[1].Value + "\n" +
+        "Origin Country: " + myMovieData.Country + "\n" +
+        "Language: " + myMovieData.Language + "\n"  +
+        "Plot: " + myMovieData.Plot + "\n" +
+        "Actors" + myMovieData.Actors + "\n" 
+
+        console.log(queryUrlResults);
+
+    } else {
+        console.log("error: " + err)
+        return;
+    }
+});
+
+}
+
+
+
 
