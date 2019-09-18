cc77341c3dfc04d6b328970bbadb79564e2f1577
Haralambos <lampistsivikas@yahoo.com>
2019-09-17 18:19:49 -0700

    new message

--- keys.js
+++ keys.js
@@ -1,8 +1,5 @@
 
-console.log('this is loaded');
-exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+  var Spotify = require('spotify-web-api-js');
+  var s = new Spotify();
+  
 
-
-  };
